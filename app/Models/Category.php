8852eca37cd88631fc039959282c0715747c2993
Rahim Uddin <rahimuddin5915@gmail.com>
2024-02-15 12:08:13 +0600

    added nid card modal and migration

--- app/Models/Category.php
+++ app/Models/Category.php
@@ -2,11 +2,16 @@
 
 namespace App\Models;
 
+use App\Http\Requests\SubcategoryStoreRequest;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
 
 class Category extends Model
 {
     use HasFactory;
-    protected $fillable =['name','slug','is_active'];
+    protected $fillable = ['name', 'slug', 'is_active'];
+    public function subcategories()
+    {
+        return $this->hasMany(SubCategory::class);
+    }
 }
